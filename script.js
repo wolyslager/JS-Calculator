02a8203e12f6799e4abe3f5348930ea061f52784
wolyslager <olyslager.willy@gmail.com>
2020-10-01 18:53:33 -0500

    added addition logic

--- script.js
+++ script.js
@@ -4,30 +4,49 @@ const divide = document.querySelector('.divide');
 const multiply = document.querySelector('.multiply');
 const subtract = document.querySelector('.subtract');
 const add = document.querySelector('.add');
+const equals = document.querySelector('.equals');
 const numberRegex = /^[0-9]*$/;
+let operatorJustClicked = false;
+let currentNumber;
+
 let expression = {
-	left: '',
-	operator: '',
-	right: ''
+	left: null,
+	operator: null, 
+	right:null
 }
 
+const operators = {
+	"+": function(x, y){return x + y}
+}
 
 const resetContent = () =>{
 	display.innerText = '0';
+	expression.left = null;
+	expression.operator = null;
+	expression.right = null;
 }
 
 resetContent();
 
 const checkDecimal = (item) => {
-	let currentNumber = display.innerText;
+	currentNumber = display.innerText;
 	const hasDecimal = currentNumber.includes('.');
+	//if not(there is a decimal and the entered text is a decimal)
 	if(!(hasDecimal && item.innerText == '.')){
-		display.textContent = currentNumber + item.innerText;
+		//if there is already a number entered, as well as an operator, clear the way for the second number
+		if (operatorJustClicked){
+			currentNumber = item.innerText;
+			display.textContent = currentNumber;
+			operatorJustClicked = false;
+		} else {
+			currentNumber = currentNumber + item.innerText;
+			display.textContent = currentNumber;
+		}
 	}
 }
 
 const checkDisplayContents = (item) => {
-	const isEmpty = !display.innerText ? true : false; 
+	const isEmpty = display.innerText == '0' ? true : false; 
 	if (isEmpty){
 		display.textContent = item.innerText;
 	} else {
@@ -37,6 +56,23 @@ const checkDisplayContents = (item) => {
 }
 
 add.addEventListener('click', function(){
+	//if there is already a number on the left and the add button is pressed 
+	if(expression.left){
+		display.textContent = Number(expression.left) + Number(currentNumber);
+	}
+	expression.operator = '+';
+	expression.left = display.innerText;
+	operatorJustClicked = true;
+})
+
+equals.addEventListener('click', function(){
+	//check if there is a left hand side to the expression
+	if (expression.left){
+		expression.right = display.innerText;
+	}
+	let left = Number(expression.left);
+	let right = Number(expression.right);
+	display.textContent = operators[expression.operator](left, right);
 	
 })
 
