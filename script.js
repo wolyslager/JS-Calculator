e8a433e1948be3eddd64e83ae3ecd3698bb4d44f
wolyslager <olyslager.willy@gmail.com>
2020-10-03 17:56:25 -0500

    implemented add, subtract, multiply, divide logic.. added styling

--- script.js
+++ script.js
@@ -6,82 +6,146 @@ const subtract = document.querySelector('.subtract');
 const add = document.querySelector('.add');
 const equals = document.querySelector('.equals');
 const numberRegex = /^[0-9]*$/;
-let operatorJustClicked = false;
 let currentNumber;
 
-let expression = {
-	left: null,
-	operator: null, 
-	right:null
+ let state = {
+ 	left: null,
+ 	operator: null, 
+ 	operatorActive:false, 
+ 	right:0, 
+ 	currentDisplayedNum: 0,
+	currentCalc: 0
 }
 
-const operators = {
-	"+": function(x, y){return x + y}
-}
+ const operators = {
+ 	"+": function(x, y){return x + y},
+ 	"-": function(x, y){return x - y}, 
+ 	"*": function(x, y){return x * y},
+ 	"/": function(x, y){return x / y}
+ }
 
-const resetContent = () =>{
-	display.innerText = '0';
-	expression.left = null;
-	expression.operator = null;
-	expression.right = null;
-}
-
-resetContent();
-
-const checkDecimal = (item) => {
-	currentNumber = display.innerText;
+ const checkOperator = (item) => {
+ 	//if there is an operator active, clear the screen for the upcoming number
+ 	if (state.operatorActive){
+ 		display.textContent = item.innerText;
+ 		state.currentDisplayedNum = display.textContent;
+ 		state.operatorActive = false;
+ 	} else {
+ 		state.currentDisplayedNum = currentNumber + item.innerText;
+		display.textContent = state.currentDisplayedNum;
+ 	}
+ }
+ 
+ const checkDecimal = (item) => {
+ 	currentNumber = display.innerText;
 	const hasDecimal = currentNumber.includes('.');
-	//if not(there is a decimal and the entered text is a decimal)
-	if(!(hasDecimal && item.innerText == '.')){
-		//if there is already a number entered, as well as an operator, clear the way for the second number
-		if (operatorJustClicked){
-			currentNumber = item.innerText;
-			display.textContent = currentNumber;
-			operatorJustClicked = false;
-		} else {
-			currentNumber = currentNumber + item.innerText;
-			display.textContent = currentNumber;
-		}
-	}
-}
+ 	if(!(hasDecimal && item.innerText == '.')){
+ 			checkOperator(item);
+ 		}
+ }
 
 const checkDisplayContents = (item) => {
-	const isEmpty = display.innerText == '0' ? true : false; 
-	if (isEmpty){
-		display.textContent = item.innerText;
-	} else {
-		//check if there is already a decimal in the innerText
-		checkDecimal(item);
-	}
-}
+ 	const isEmpty = display.innerText ==  0 ? true : false; 
+ 	if (isEmpty){
+ 		display.textContent = item.innerText;
+ 	} else {
+ 		checkDecimal(item);
+ 	}
+ }
 
-add.addEventListener('click', function(){
-	//if there is already a number on the left and the add button is pressed 
-	if(expression.left){
-		display.textContent = Number(expression.left) + Number(currentNumber);
-	}
-	expression.operator = '+';
-	expression.left = display.innerText;
-	operatorJustClicked = true;
-})
-
-equals.addEventListener('click', function(){
-	//check if there is a left hand side to the expression
-	if (expression.left){
-		expression.right = display.innerText;
-	}
-	let left = Number(expression.left);
-	let right = Number(expression.right);
-	display.textContent = operators[expression.operator](left, right);
-	
-})
-
-reset.addEventListener('click', function(){
-	resetContent();
-})
-
-const numbers = document.querySelectorAll('.num').forEach(item => {
+ const numbers = document.querySelectorAll('.num').forEach(item => {
 	item.addEventListener('click', function(){
 		checkDisplayContents(item);
-	})
-})
+ 	})
+ })
+
+
+ add.addEventListener('click', function(){
+ 	//case: there is already a state.left.. if there is then we have to 
+ 	if(state.operator){
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 		state.operator = '+';
+ 	} else {
+ 		state.operator = "+";
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 	}
+ 	state.operatorActive = true;
+
+ 	if(state.left){
+ 		state.left = state.currentCalc;
+ 		display.innerText = state.currentCalc;
+ 	} else {
+ 		state.left = display.innerText;
+ 	}
+ 	console.log(state.currentCalc);
+ 	console.log(state);
+ })
+
+ subtract.addEventListener('click', function(){
+ 	if(state.operator){
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 		state.operator = '-';
+ 	} else {
+ 		state.operator = "-";
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 	}
+ 	state.operatorActive = true;
+
+ 	if(state.left){
+ 		state.left = state.currentCalc;
+ 		display.innerText = state.currentCalc;
+ 	} else {
+ 		state.left = display.innerText;
+ 	}
+ 	console.log(state.currentCalc);
+ 	console.log(state)
+ 	
+ })
+
+ multiply.addEventListener('click', function(){
+ 	if(state.operator){
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 		state.operator = '*';
+ 	} else {
+ 		state.operator = "*";
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 	}
+ 	state.operatorActive = true;
+
+ 	if(state.left){
+ 		state.left = state.currentCalc;
+ 		display.innerText = state.currentCalc;
+ 	} else {
+ 		state.left = display.innerText;
+ 	}
+ 	console.log(state.currentCalc);
+ 	console.log(state)
+ })
+
+  divide.addEventListener('click', function(){
+ 	if(state.operator){
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 		state.operator = '/';
+ 	} else {
+ 		state.operator = "/";
+ 		state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+ 	}
+ 	state.operatorActive = true;
+
+ 	if(state.left){
+ 		state.left = state.currentCalc;
+ 		display.innerText = state.currentCalc;
+ 	} else {
+ 		state.left = display.innerText;
+ 	}
+ 	console.log(state.currentCalc);
+ 	console.log(state)
+ })
+
+  equals.addEventListener('click', function(){
+  	state.currentCalc = operators[state.operator](Number(state.left), Number(state.currentDisplayedNum));
+	display.innerText = state.currentCalc;
+	
+  })
+
+
